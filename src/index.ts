996036034b9d8adc7deba52e15487ec61f45b7cb
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-07 13:45:21 -0500

    Implement stdin parsing and outputting the list of colors to mix.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("wew lad");
+import getStdin from "get-stdin";
+import * as process from "process";
+
+import { parse, Order, ParseError, ParseErrorCode } from "./parser";
+
+(async () => {
+    let order: Order;
+    try {
+        order = parse(await getStdin());
+    } catch (err) {
+        if (!(err instanceof ParseError)) {
+            console.error(err.message);
+            process.exit(1);
+
+            return;
+        }
+
+        if (err.code === ParseErrorCode.OverlappingPaintType) {
+            console.error("No solution exists");
+        } else {
+            console.error(err.message);
+        }
+
+        process.exit(1);
+
+        return;
+    }
+
+    const output: string[] = Object.keys(order.colors).map((key) => {
+        return order.colors[Number(key)];
+    });
+    console.log(output.join(" "));
+})();
